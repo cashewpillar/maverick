b93b68bc6b59ad5e220bf7869787011b7176238c
cashewpillar <cashewpillar@gmail.com>
2024-06-19 20:19:32 +0800

    refactor: use postgres

--- app/core/config.py
+++ app/core/config.py
@@ -1,19 +1,36 @@
 import secrets
 from typing import Annotated, Literal
 
-from pydantic import AnyUrl, BeforeValidator, computed_field
+from pydantic import ( 
+    PostgresDsn, 
+    AnyUrl, 
+    BeforeValidator, 
+    computed_field
+)
+from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from app.utils import parse_cors
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=".env", env_ignore_empty=True, extra="ignore"
-    )
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     DOMAIN: str = "localhost"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+    BACKEND_CORS_ORIGINS: Annotated[
+        list[AnyUrl] | str, BeforeValidator(parse_cors)
+    ] = []
+
+    PROJECT_NAME: str
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str = ""
+    POSTGRES_PORT: str
+    
+    model_config = SettingsConfigDict(
+        env_file=".env", env_ignore_empty=True, extra="ignore"
+    )
 
     @computed_field  # type: ignore[misc]
     @property
@@ -23,10 +40,16 @@ class Settings(BaseSettings):
             return f"http://{self.DOMAIN}"
         return f"https://{self.DOMAIN}"
 
-    BACKEND_CORS_ORIGINS: Annotated[
-        list[AnyUrl] | str, BeforeValidator(parse_cors)
-    ] = []
-
-    PROJECT_NAME: str
+    @computed_field  # type: ignore[misc]
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            schceme="postgresql+psycopg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
+            database=self.POSTGRES_DB
+        )
 
 settings = Settings()  # type: ignore
